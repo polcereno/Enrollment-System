c9a20234885da81b6a6b8febfd59126be3d09b38
polcereno <paulcerenopaul@gmail.com>
2024-07-05 17:29:07 +0800

    test

---
+++ app/src/main/java/com/example/enrollment_system/student_ui/due_amount/DueAmountActivity.java
@@ -0,0 +1,26 @@
+package com.example.enrollment_system.student_ui.due_amount;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.enrollment_system.R;
+
+public class DueAmountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_due_amount);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
